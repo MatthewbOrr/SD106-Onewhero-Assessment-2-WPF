b9fbcd2a9285f44aeb13cf77271b3db772678685
Han_Yoo <poxsstar@gmail.com>
2025-11-10 03:10:13 +1300

    SQL Database update

--- Model/User.cs
+++ Model/User.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SD106_Onewhero_Assessment_2.Model
 {
-    class Class1
+    public class User
     {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
     }
 }
